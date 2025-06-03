b5e160b1b6143a0ee1a053b08c16316dc57c5913
fenmgshan <wangtianlin2005@gmail.com>
2025-06-03 16:17:19 +0800

    游戏开始界面

---
+++ src/StartMenuState.cpp
@@ -0,0 +1,22 @@
+#include "GameState.h"
+#include "StartMenuState.h"
+#include "TetrisGame.h"
+
+StartMenuState::StartMenuState(sf::RenderWindow& window){}
+
+void StartMenuState::handleEvent(sf::Event& event) {
+    /*
+    这一块是处理开始菜单的事件，比如点击开始按钮等
+    wait for you to implement the event handling logic!
+    */
+}
+void StartMenuState::update(std::unique_ptr<GameState>& currentState) {
+    // 直接进入游戏状态，等待你的后续修改。
+    currentState = std::make_unique<TetrisGame>();
+}
+void StartMenuState::render(sf::RenderWindow& window) {
+    /*
+    这一块是渲染开始菜单，比如渲染标题和开始按钮等
+    wait for you to implement the rendering logic!
+    */
+}
